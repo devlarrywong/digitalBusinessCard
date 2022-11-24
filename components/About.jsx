f1491f26fe173f2ba79e31d94bf34c955c0a444f
Larry Wong <dev.larry.wong@gmail.com>
2022-11-24 14:25:32 +0000

    added About and Interests component and refine CSS

---
+++ components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './section.css';
+
+function About() {
+  return (
+    <section>
+      <h2 className="About__title">About</h2>
+      <p>
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </section>
+  );
+}
+
+export default About;
---
+++ components/Interests.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './section.css';
+
+function Interests() {
+  return (
+    <section>
+      <h2>Interests</h2>
+      <p className="Interests__details">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </section>
+  );
+}
+
+export default Interests;
